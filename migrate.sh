2b5b26f9071b937239c678b1a4d71e000c2e0632
Roel Theeuwen <roel@angela-ThinkPad-P50>
2017-10-19 16:33:32 +0200

    initial commit

--- migrate.sh
+++ migrate.sh
@@ -2,11 +2,10 @@
 PROJECT=$(echo $1 | sed 's/\/*$//g')
 cd $PROJECT
 mkdir $PROJECT
-find . -maxdepth 1 ! -name $PROJECT |grep -v '^\./\.' | grep -v '^\.$' | tee | xargs -I '{}' ~/gitrewrite.sh '{}'=$PROJECT/'{}'
+find . -maxdepth 1 ! -name $PROJECT | grep -v '^\./\.' | grep -v '^\.$' | xargs -I '{}' ~/gitrewrite.sh '{}'=$PROJECT/'{}'
 git commit -m "preparing for directory migration"
 cd ..
 git pull $PROJECT --allow-unrelated-histories 
-xdotool key "control+x"
 rm $PROJECT/$PROJECT -rf
 rm $PROJECT/.git -rf
 
